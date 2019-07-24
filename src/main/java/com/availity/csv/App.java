6eef140ee1a62ccdbd5630580352bb0d36e2489a
Sergiy Velytskyy <velitski@yahoo.ca>
2019-07-24 01:42:23 -0400

    Code cleanup

--- src/main/java/com/availity/csv/App.java
+++ src/main/java/com/availity/csv/App.java
@@ -7,7 +7,6 @@ import org.apache.log4j.Logger;
 
 import com.availity.csv.processor.CsvLoader;
 import com.availity.csv.processor.CsvReader;
-import com.availity.csv.processor.FileProcessor;
 import com.availity.csv.utils.CsvUtils;
 
 
@@ -18,20 +17,19 @@ public class App
     public static void main( String[] args )
     {
         CsvLoader loader = new CsvLoader();
-        //String csvFile = "file1.csv";
         Properties prop = CsvUtils.loadProperty();
-        //String path = "D:\\Projects\\eclipse\\TestAvaility\\files\\";
         String path = prop.getProperty("insurance.folder.root");
         List<String>csvFiles = CsvUtils.getListOfCsvFiles(path);
         for(String csvFile : csvFiles) {
         	loader.loadcsv(csvFile, path, 1000);
         }
-        FileProcessor processor = new FileProcessor();
         log.debug(csvFiles);
-        processor.processFile(path+"stage"+"\\BCBSFL.csv", path);
-        CsvReader reader = new CsvReader();
-        reader.sortAndRemoveDuplicates(path+"stage"+"\\BCBSFL.csv", path);
-        //loader.apacheloadcsv(csvFile, path);
+        List<String>csvStageFiles = CsvUtils.getListOfCsvFiles(path+"\\stage\\");
+        log.debug(csvStageFiles);
+        for(String csvFile : csvStageFiles) {
+        	CsvReader reader = new CsvReader();
+        	reader.sortAndRemoveDuplicates(path+"stage\\"+csvFile, path);
+        }
     }
     
     
